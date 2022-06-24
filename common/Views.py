59d669525a8fa135d16904147f3fbb53e1b896fb
syitzhm <87540223+syitzhm@users.noreply.github.com>
2022-06-24 00:19:05 -0700

    6.23

--- common/Views.py
+++ common/Views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.template.context_processors import request
 from django.views import generic
+from .models import mainclouth
 
 
 # def Index(request):
@@ -9,8 +10,14 @@ from django.views import generic
     # template_name = '../templates/common/index.html'
     # context_object_name = 'timeline_list'
     #
-class Index(generic.TemplateView):
+class Index(generic.ListView):
+    model=mainclouth
     template_name = 'common/index.html'
+    context_object_name = 'mainclouths'
+
+    def get_queryset(self):  # 重写get_queryset方法
+        # 获取所有is_main为True的clouth，并且以时间倒序返回数据
+        return mainclouth.objects.filter(is_main= True).order_by('-create_date')
 
 def Single(request):
     return render(request,'common/single.html')
